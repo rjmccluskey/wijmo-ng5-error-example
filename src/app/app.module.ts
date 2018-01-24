610fc71213ed04dd01c19831ef05d58643a10e22
Rich McCluskey <richard.mccluskey@hciq.com>
2018-01-23 16:53:31 -0800

    call CollectionView to cause wijmo error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { CollectionView } from 'wijmo/wijmo';
 
+new CollectionView();
 
 @NgModule({
   declarations: [
